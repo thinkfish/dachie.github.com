ffda5e392723ae659a928f05e7119abf6340d1d8
dachie <dachie@qq.com>
2013-08-10 22:44:31 +0800

    update clone .slice(0)

--- demo/flow/flow.js
+++ demo/flow/flow.js
@@ -18,6 +18,7 @@ Flow.prototype={
 		for (var i = 0; i < this.count; i++) {
 			var obj = {
 				background:"rgb("+this.random(0,255)+","+this.random(0,255)+","+this.random(0,255)+")",
+				/* '#' + (Math.random() * 0xffffff << 0).toString(16)*/
 				height:this.random(100,200)+"px"
 			};
 			this.arr.push(obj);
@@ -26,17 +27,29 @@ Flow.prototype={
 	},
 
 	render:function(){
-		for (var i = 0; i < this.arr.length; i++) {
+		var that = this;
+		var els = document.createDocumentFragment();
+		var i = 0;
+		var timer = setInterval(function(){
 			var el = document.createElement("div");
 			el.innerHTML=i+1;
-			el.style.backgroundColor = this.arr[i].background;
-			el.style.height = this.arr[i].height;
-			var p = this.getPosition();
+			el.style.backgroundColor = that.arr[i].background;
+			el.style.height = that.arr[i].height;
+			var p = that.getPosition();
 			el.style.top=p.top;
 			el.style.left = p.left;
-			this.addCache(el);
-			document.getElementById("flow1").appendChild(el);
-		}
+			that.addCache(el);
+			els.appendChild(el);
+			i++;
+			//if(i%that.cols==0){
+				document.getElementById("flow1").appendChild(els);
+				els = document.createDocumentFragment();
+			//}
+			if(!(i<that.arr.length)){
+				clearInterval(timer);
+				document.getElementById("flow1").appendChild(els);
+			}
+		}, 300);
 	},
 
 	addCache:function(el){
@@ -56,7 +69,7 @@ Flow.prototype={
 		if(l<this.cols){
 			return {top:this.cache[l-1].style.top,left:(parseInt(this.cache[l-1].style.left)+960/this.cols)+"px"};
 		}else{
-			var _cache = this.cache;
+			var _cache = this.cache.slice(0);
 			_cache.sort(function(x,y){
 				var _x = parseInt(x.style.top)+parseInt(x.style.height);
 				var _y = parseInt(y.style.top)+parseInt(y.style.height);
